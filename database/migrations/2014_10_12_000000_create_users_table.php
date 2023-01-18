ac1b9d51407484c182e1ff73438cf8df5b1e746c
Satrya Wiguna <satrya@freshcms.net>
2023-01-18 18:53:57 +0800

    JIT-0005: Add migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,20 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id', 36)->primary();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->unsignedSmallInteger('role_id');
+
+            $table->foreign('role_id')->references('id')->on('roles')
+                ->onDelete('restrict');
         });
     }
 
